c1e690293f061fe06f493ab048a399f0b476f4bc
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-01-22 15:11:14 +0200

    WIP: Implements sparse matrix multiplication and p1 parameter calculation

--- src/main.cu
+++ src/main.cu
@@ -2,6 +2,11 @@
 #include <cuda_runtime.h>
 #include "../headers/mtx.hpp"
 
+__global__ void sparseMatrixMult(CSRMatrix *A, CSCMatrix *B, CSRMatrix *C);
+
+__global__ void hadamardProduct(int *A, int *B);
+
+__host__ void makeUnit(CSCMatrix *A, int rows);
 
 int main(int argc, char* argv[])
 {
@@ -29,8 +34,41 @@ int main(int argc, char* argv[])
     readMTX(adjacent, argv[1]);
     cudaMemcpy(devAdjacent, adjacent, sizeof(CSRMatrix), cudaMemcpyHostToDevice);
 
+    // ~~~~~~~ calculate p1
+    makeUnit(unitVector, adjacent->columns);
+    cudaMemcpy(devUnitVector, unitVector, sizeof(CSCMatrix), cudaMemcpyHostToDevice);
+    blocksPerGrid = (adjacent->rows + threadsPerBlock - 1) / threadsPerBlock;
+    sparseMatrixMult<<<blocksPerGrid, threadsPerBlock>>>(devAdjacent, devUnitVector, devp1);
+
     std::cout << "#Rows/Columns: " << adjacent->rows << std::endl;
     std::cout << "#Non-zeros: " << adjacent->nz << std::endl;
 
     return 0;
+}
+
+__global__ void sparseMatrixMult(CSRMatrix *A, CSCMatrix *B, CSRMatrix *C)
+{
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (row < A->rows && col < B->columns)
+    {
+
+    }
+
+    printf("Row: %d, Col: %d\n", row, col);
+
+}
+
+__host__ void makeUnit(CSCMatrix *A, int rows)
+{
+    A = new CSCMatrix(1, rows, rows);
+    A->colIndex[0] = 0;
+    A->colIndex[1] = A->nz;
+
+    for (int i = 0; i < A->nz; i++)
+    {
+        A->nzIndex[i] = i;
+        A->nzValues[i] = 1;
+    }
 }
