d12b25f660bd17e4480041bab02f880807dce4d5
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-01-22 15:09:48 +0200

    Declared variables and allocates memory to host and device

--- src/main.cu
+++ src/main.cu
@@ -11,10 +11,26 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    CSRAdjacentMatrix adjacent = readMTX(argv[1]);
+    // variable declaration and memory allocation
+    CSRMatrix *adjacent, *p1;
+    CSRMatrix *devAdjacent, *devp1;
+    CSCMatrix *unitVector;
+    CSCMatrix *devUnitVector;
+    int blocksPerGrid, threadsPerBlock = 256;
 
-    std::cout << "#Rows/Columns: " << adjacent.size << std::endl;
-    std::cout << "#Non-zeros: " << adjacent.nz << std::endl;
+    cudaMallocHost(&adjacent, sizeof(CSRMatrix));
+    cudaMallocHost(&p1, sizeof(CSRMatrix));
+    cudaMallocHost(&unitVector, sizeof(CSCMatrix));
+    cudaMalloc(&devAdjacent, sizeof(CSRMatrix));
+    cudaMalloc(&devp1, sizeof(CSRMatrix));
+    cudaMalloc(&devUnitVector, sizeof(CSCMatrix));
+
+    // get adjacent matrix and copy to GPU
+    readMTX(adjacent, argv[1]);
+    cudaMemcpy(devAdjacent, adjacent, sizeof(CSRMatrix), cudaMemcpyHostToDevice);
+
+    std::cout << "#Rows/Columns: " << adjacent->rows << std::endl;
+    std::cout << "#Non-zeros: " << adjacent->nz << std::endl;
 
     return 0;
 }
