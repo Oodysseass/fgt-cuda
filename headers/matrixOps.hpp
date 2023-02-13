9d510ec5fff09a1d3117855c4578945c2afbc231
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-02-13 22:03:34 +0200

    Moves memory allocation to different src file

---
+++ headers/matrixOps.hpp
@@ -0,0 +1,31 @@
+#ifndef MATRIXOPS_HPP
+#define MATRIXOPS_HPP
+
+#include <cuda_runtime_api.h>
+#include <cusparse.h>
+#include "mtx.hpp"
+
+
+#define CHECK_CUDA(func)                                                       \
+{                                                                              \
+    cudaError_t status = (func);                                               \
+    if (status != cudaSuccess) {                                               \
+        printf("CUDA API failed at line %d with error: %s (%d)\n",             \
+               __LINE__, cudaGetErrorString(status), status);                  \
+        exit(1);                                                               \
+    }                                                                          \
+}
+
+#define CHECK_CUSPARSE(func)                                                   \
+{                                                                              \
+    cusparseStatus_t status = (func);                                          \
+    if (status != CUSPARSE_STATUS_SUCCESS) {                                   \
+        printf("CUSPARSE API failed at line %d with error: %s (%d)\n",         \
+               __LINE__, cusparseGetErrorString(status), status);              \
+        exit(1);                                                               \
+    }                                                                          \
+}
+
+void sparseMult(CSRMatrix *A, CSRMatrix *B, CSRMatrix *C);
+
+#endif
