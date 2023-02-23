6f3e4b585b913e613eae7178ad05ba89332ec89d
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-02-23 14:43:53 +0200

    Implements reading of symmetric matrixes

--- src/mtx.cu
+++ src/mtx.cu
@@ -31,7 +31,8 @@ CSCMatrix readMTX(std::string filename)
     }
 
     char mmx[20], b1[20], b2[20], b3[20], b4[20];
-    int temp, numCols, numNZ;
+    int numRows, numCols, numNZ, size;
+    bool isSymmetric;
 
     // read banner
     fin >> mmx >> b1 >> b2 >> b3 >> b4;
@@ -53,26 +54,98 @@ CSCMatrix readMTX(std::string filename)
         exit(1);
     }
 
+    if (!strcmp(b4, "symmetric"))
+        isSymmetric = true;
+
     // skip comment
     fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     while (fin.peek() == '%')
         fin.ignore(2048, '\n');
 
     // read defining parameters
-    fin >> temp >> numCols >> numNZ;
+    fin >> numRows >> numCols >> numNZ;
 
-    // convert coo to csc while reading
-    CSCMatrix cscAdj = CSCMatrix(numCols, temp, numNZ);
+    if (isSymmetric)
+        size = 2 * numNZ;
+    else
+        size = numNZ;
+
+    // allocate space for COO format
+    int *rowcoo = new int[size];
+    int *colcoo = new int[size];
+
+    // read the COO data
+    int j = 0;
     for (int i = 0; i < numNZ; i++)
     {
-        fin >> cscAdj.nzIndex[i] >> temp;
-        cscAdj.colIndex[temp]++;
-        cscAdj.nzIndex[i]--;
+        fin >> rowcoo[j] >> colcoo[j];
+
+        if (isSymmetric)
+            // we do not keep self-loop, remove edges
+            if (rowcoo[j] == colcoo[j])
+                size -= 2;
+
+            // put symmetric edge
+            else
+            {
+                rowcoo[j + 1] = colcoo[j];
+                colcoo[j + 1] = rowcoo[j];
+                j += 2;
+            }
+        // we do not keep self-loop, remove edge
+        else if (rowcoo[j] == colcoo[j])
+            size -= 1;
+        else
+            j++;
+    }
+
+    numNZ = size;
+    rowcoo = static_cast<int *>(realloc(rowcoo, numNZ * sizeof(int)));
+    colcoo = static_cast<int *>(realloc(colcoo, numNZ * sizeof(int)));
+
+    // close connection to file
+    fin.close();
+
+    // convert coo to csc
+    CSCMatrix cscAdj = CSCMatrix(numCols, numRows, numNZ);
+
+    // find column sizes
+    for (int i = 0; i < numNZ; i++)
+    {
+        cscAdj.colIndex[colcoo[i] - 1]++;
         cscAdj.nzValues[i] = 1;
     }
 
-    for (int i = 0; i < numCols; i++)
-        cscAdj.colIndex[i + 1] += cscAdj.colIndex[i];
+    for (int i = 0, cumsum = 0; i < numRows; i++)
+    {
+        int temp = cscAdj.colIndex[i];
+        cscAdj.colIndex[i] = cumsum;
+        cumsum += temp;
+    }
+    cscAdj.colIndex[numCols] = numNZ;
+
+    // copy row indices to correct place
+    for (int i = 0; i < numNZ; i++)
+    {
+        int coli = colcoo[i] - 1;
+        int dest = cscAdj.colIndex[coli];
+        cscAdj.nzIndex[dest] = rowcoo[i] - 1;
+
+        cscAdj.colIndex[coli]++;
+    }
+
+    // revert column pointers
+    for (int i = 0, last = 0; i < numCols; i++)
+    {
+        int temp = cscAdj.colIndex[i];
+        cscAdj.colIndex[i] = last;
+
+        last = temp;
+    }
+
+    // deallocate
+    free(rowcoo);
+    free(colcoo);
 
     return cscAdj;
 }
