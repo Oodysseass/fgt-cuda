f6502d9d6c58ad01930f6277483395d4853127fc
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-02-26 20:30:32 +0200

    Changes scripts for new names

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -6,6 +6,6 @@
 #SBATCH --time=10:00
 
 module load gcc/7.3.0 cuda/10.0.130
-nvcc src/main.cu src/mtx.cu src/matrixOps.cu -O3 -o main
+nvcc src/main.cu src/mtx.cu src/fglt.cu -O3 -o main
 
 ./main auto.mtx
